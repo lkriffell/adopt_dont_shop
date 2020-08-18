74dfa5ff28d3e9beba8b1504571b472a2f4042d3
Logan Riffell <lkriffell@gmail.com>
2020-08-17 19:45:39 -0600

    Add first 4 stories

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/shelters', to: 'shelters#index'
+  get '/shelters/:id', to: 'shelters#show'
+  get '/shelters/new', to: 'shelters#new'
+  post '/shelters', to: 'shelters#create'
+  get '/shelters/:id/edit', to: 'shelters#edit'
+  patch '/shelters', to: 'shelters#update'
 end
