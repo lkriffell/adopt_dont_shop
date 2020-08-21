8a57b26223efe42bcc089b75a79eca75e0326211
Logan Riffell <lkriffell@gmail.com>
2020-08-20 18:09:52 -0600

    Remove unneeded validations

--- spec/models/pets_spec.rb
+++ spec/models/pets_spec.rb
@@ -9,9 +9,6 @@ describe Pet, type: :model do
     it { should validate_presence_of :name }
     it { should validate_presence_of :sex }
     it { should validate_presence_of :approximate_age }
-    it { should validate_presence_of :adoption_status }
-    it { should validate_presence_of :current_location }
-    it { should validate_presence_of :shelter_id }
   end
 
   describe 'relationships' do
