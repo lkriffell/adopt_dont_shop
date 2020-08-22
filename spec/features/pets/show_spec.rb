4d5a641830e69bea5e007fbf33ee9c9fd2e675da
Logan Riffell <lkriffell@gmail.com>
2020-08-22 12:06:56 -0600

    Refactor for neatness

--- spec/features/pets/show_spec.rb
+++ spec/features/pets/show_spec.rb
@@ -3,18 +3,26 @@ require 'rails_helper'
 # Before each for setup
 RSpec.describe 'pet show displays' do
   before :each do
+    @shelter1 = Shelter.create!(name: "Alfredo's Adoption",
+                                address: "55555",
+                                city: "Denver",
+                                state: "CO",
+                                zip: "34213",
+                                id: "1")
+    @pet1 = Pet.create!(name: "Jimbo",
+                        approximate_age: "1",
+                        sex: "male",
+                        image: "jimbo.jpg",
+                        adoption_status: "Adoptable",
+                        current_location: "Alfredo's Adoption",
+                        shelter_id: "1")
   end
 
   it 'pet attributes displayed' do
-    @shelter1 = Shelter.create(name: "Alfredo's Adoption", address: "55555",
-                              city: "Denver", state: "CO", zip: "34213", id: "1")
-    @pet1 = Pet.create(name: "Jimbo", approximate_age: "1", sex: "male", image: "jimbo.jpg",
-                      adoption_status: "Adoptable", current_location: "Alfredo's Adoption", shelter_id: "1", id: "1")
     visit "pets/1"
 
     expect(page).to have_content(@pet1.name)
     expect(page).to have_content(@pet1.approximate_age)
-    expect(page).to have_content(@pet1.image)
     expect(page).to have_content(@pet1.sex)
     expect(page).to have_content(@pet1.current_location)
   end
--- spec/features/shelters/show_pets_spec.rb
+++ spec/features/shelters/show_pets_spec.rb
@@ -3,20 +3,27 @@ require 'rails_helper'
 # Before each for setup
 RSpec.describe 'shelters show page' do
   before :each do
+    @shelter1 = Shelter.create!(name: "Alfredo's Adoption",
+                                address: "55555",
+                                city: "Denver",
+                                state: "CO",
+                                zip: "34213",
+                                id: "1")
+    @pet1 = Pet.create!(name: "Jimbo",
+                        approximate_age: "1",
+                        sex: "male",
+                        image: "jimbo.jpg",
+                        adoption_status: "Adoptable",
+                        current_location: "Alfredo's Adoption",
+                        shelter_id: "1")
   end
 
   it 'displays pet attributes' do
-    @shelter1 = Shelter.create(name: "Alfredo's Adoption", address: "55555",
-                              city: "Denver", state: "CO", zip: "34213", id: "1")
-    @pet1 = Pet.create(name: "Jimbo", approximate_age: "1", sex: "male", image: "jimbo.jpg",
-                      adoption_status: "Adoptable", current_location: "Alfredo's Adoption", shelter_id: "1")
-
     visit "/shelters/#{@shelter1.id}/pets"
 
     expect(page).to have_content(@shelter1.name)
     expect(page).to have_content(@pet1.name)
     expect(page).to have_content(@pet1.approximate_age)
-    expect(page).to have_img(@pet1.image)
     expect(page).to have_content(@pet1.sex)
 
   end
