ae06a34ca02dce9d8fafb707476af32ec9f62899
Logan Riffell <lkriffell@gmail.com>
2020-08-23 15:01:31 -0600

    Comment out code to since mobile style features isn't complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,16 @@
 class ApplicationController < ActionController::Base
-  before_action :detect_device_type
-
-  private
-
-    def detect_device_type
-      case request.user_agent
-      when /iPhone/i
-        request.variant = :phone
-      when /Android/i && /mobile/i
-        request.variant = :phone
-      when /Windows Phone/i
-        request.variant = :phone
-      end
-    end
+  # before_action :detect_device_type
+  #
+  # private
+  #
+  #   def detect_device_type
+  #     case request.user_agent
+  #     when /iPhone/i
+  #       request.variant = :phone
+  #     when /Android/i && /mobile/i
+  #       request.variant = :phone
+  #     when /Windows Phone/i
+  #       request.variant = :phone
+  #     end
+  #   end
 end
