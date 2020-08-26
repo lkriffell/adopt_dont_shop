8734df89177d3d7833e76e262aa62f2513039c0d
Logan Riffell <lkriffell@gmail.com>
2020-08-25 19:58:15 -0600

    Add working code through user story 12

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+@shelter1 = Shelter.create!(name: "Alfredo's Adoption",
+                            address: "55555",
+                            city: "Denver",
+                            state: "CO",
+                            zip: "34213",
+                            id: "1")
+@pet1 = Pet.create!(name: "Jimbo",
+                    approximate_age: "1",
+                    sex: "male",
+                    image: "jimbo.jpg",
+                    adoption_status: "Adoptable",
+                    current_location: "Alfredo's Adoption",
+                    shelter_id: "1")
+@review = @shelter1.reviews.create!(title: "Terrible Service",
+                                  rating: 1,
+                                  content: "We did eight hours just to get my dog!! And when I got him he was covered in chocolate syrup :(")
